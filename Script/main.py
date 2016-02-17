4315467b3a8b4e976bdf6613f57a5f8597a2d131
cclauss <cclauss@bluewin.ch>
2016-02-17 13:54:57 +0100

    We have to be ready for Python3 ;-)

--- Script/main.py
+++ Script/main.py
@@ -3,5 +3,4 @@
 # Put your script (and all resources that it needs for running) in this folder.
 # The filename of the main script should be "main.py".
 
-print 'Hello World'
-
+print('Hello World')
